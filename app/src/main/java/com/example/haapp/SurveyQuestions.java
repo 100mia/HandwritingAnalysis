f19aa1c652f1030eb5d1df3eec99b3b8db15e2fa
SanyaGarg <gargsanya828@gmail.com>
2021-10-13 01:16:53 +0530

    Second Commit

--- app/src/main/java/com/example/haapp/SurveyQuestions.java
+++ app/src/main/java/com/example/haapp/SurveyQuestions.java
@@ -3,6 +3,7 @@ package com.example.haapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SurveyQuestions extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SurveyQuestions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_questions);
+    }
+
+    public void onRadioButtonClicked(View view) {
     }
 }
